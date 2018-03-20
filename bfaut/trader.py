a1d6096b3fd48a443c5350d8f4ef742117ea53c5
dceoy <dnarsil+github@gmail.com>
2018-03-20 23:21:04 +0900

    Update judgment conditions

--- bfaut/trader.py
+++ bfaut/trader.py
@@ -28,11 +28,12 @@ class BfStreamTrader(SubscribeCallback):
         self.sfd_pins = np.array([0.05, 0.1, 0.15, 0.2])
         self.contrary = (self.mode == 'negative')               # mutable
         self.ticks = {}                                         # mutable
-        self.open = True                                        # mutable
+        self.open = None                                        # mutable
         self.won = False                                        # mutable
         self.n_load = 20                                        # mutable
         self.ewm_dv = {'mean': 0, 'var': 1}                     # mutable
-        self.ewm_pl = {'mean': 0, 'var': 1}                     # mutable
+        self.bollinger_band = np.array([])                      # mutable
+        self.ewm_pl = {'mean': 0, 'var': 1000}                  # mutable
         self.order_side = None                                  # mutable
         self.margin = None                                      # mutable
         self.position = {}                                      # mutable
@@ -41,7 +42,7 @@ class BfStreamTrader(SubscribeCallback):
         self.reserved = {}                                      # mutable
         self.order_datetime = None                              # mutable
         self.last_open = {}                                     # mutable
-        self.anchor_margin = 0                                  # mutable
+        self.retried_side = None                               # mutable
         self.n_size_over = 0                                    # mutable
         self.logger.debug(vars(self))
 
@@ -53,6 +54,11 @@ class BfStreamTrader(SubscribeCallback):
                 pd.DataFrame({'side': ['BUY', 'SELL'], 'size': [0, 0]})
             ).groupby('side')['size'].sum()
             self.ewm_dv = self._compute_ewm_delta_volume()
+            self.bollinger_band = (
+                self.ewm_dv['mean'] + np.array([- 1, 1]) *
+                np.sqrt(self.ewm_dv['var']) *
+                (self.trade.get('sigma_trigger') or 0)
+            )
             try:
                 self.margin = self._fetch_margin()
             except Exception as e:
@@ -60,15 +66,7 @@ class BfStreamTrader(SubscribeCallback):
             else:
                 if self.n_load <= 0:
                     self.order_side = self._determine_order_side()
-                    if self.order_side is None:
-                        self._print('Skip by volume difference.')
-                    elif self.order_side == self.reserved.get('side'):
-                        self._print(
-                            'Skip by position. (side: {0}, size: {1})'.format(
-                                self.reserved['side'], self.reserved['size']
-                            )
-                        )
-                    else:
+                    if self.order_side:
                         try:
                             self.position = self._fetch_position()
                             self.sfd_penal_side = self._fetch_sfd_penal_side()
@@ -77,6 +75,25 @@ class BfStreamTrader(SubscribeCallback):
                         else:
                             self.logger.debug(self.position)
                             self._trade()
+                    else:
+                        self._print(
+                            'Skip by delta volume. (band: {})'.format(
+                                np.array2string(
+                                    self.bollinger_band,
+                                    formatter={
+                                        'float_kind':
+                                        lambda f: '{:8.4f}'.format(f)
+                                    }
+                                )
+                            )
+                        )
+                elif self.n_load == 1:
+                    try:
+                        self.reserved = self._fetch_position()
+                    except Exception as e:
+                        self.logger.error(e)
+                    else:
+                        self.n_load -= 1
                 else:
                     self.n_load -= 1
                     self._print(
@@ -94,9 +111,8 @@ class BfStreamTrader(SubscribeCallback):
 
     def _print(self, message):
         text = (
-            '| BUY:{0} | SELL:{1} | EWM DELTA:{2:8.3f} | MARGIN:  {3} | ' +
-            'EWM PL:{4:>8d} | ANCHOR:{5:>' + str(len(str(int(self.margin))) + 2) +
-            'd} |\t> {6}'
+            '| BUY:{0} | SELL:{1} | EWM DELTA:{2:8.3f} | MARGIN:  {3} |' +
+            '{4}\t> {5}'
         ).format(
             *[
                 (
@@ -105,14 +121,33 @@ class BfStreamTrader(SubscribeCallback):
                 ) for s in ['BUY', 'SELL']
             ],
             self.ewm_dv['mean'], round(self.margin),
-            round(self.ewm_pl['mean']),
-            (self.anchor_margin and round(self.margin - self.anchor_margin)),
+            (
+                ' EWM PL:{:>8d} |'.format(round(self.ewm_pl['mean']))
+                if self.trade.get('mode') == 'pivot' else ''
+            ),
             message
         )
         if self.quiet:
             self.logger.info(text)
         else:
             print(text, flush=True)
+
+    def _determine_order_side(self):
+        if min(self.bollinger_band) > 0:
+            fw_side = 'BUY'
+        elif max(self.bollinger_band) < 0:
+            fw_side = 'SELL'
+        elif self.reserved.get('side') == 'BUY' and self.ewm_dv['mean'] <= 0:
+            fw_side = 'SELL'
+        elif self.reserved.get('side') == 'SELL' and self.ewm_dv['mean'] > 0:
+            fw_side = 'BUY'
+        else:
+            fw_side = None
+        order_side = self.retried_side or (
+            self._reverse_side(fw_side) if self.contrary else fw_side
+        )
+        self.logger.info('order_side: {}'.format(order_side))
+        return order_side
 
     def _fetch_margin(self):
         collateral = self.bF.getcollateral()
@@ -198,68 +233,36 @@ class BfStreamTrader(SubscribeCallback):
         self.logger.info('ewm_pl: {}'.format(ewm_pl))
         return ewm_pl
 
-    def _determine_order_side(self):
-        bollinger_band = (
-            self.ewm_dv['mean'] + np.array([- 1, 1]) *
-            np.sqrt(self.ewm_dv['var']) *
-            (self.trade.get('sigma_trigger') or 0)
-        )
-        if min(bollinger_band) > 0:
-            fw_side = 'BUY'
-        elif max(bollinger_band) < 0:
-            fw_side = 'SELL'
-        elif self.ewm_dv['mean'] > 0:
-            fw_side = 'SELL'
-        elif self.ewm_dv['mean'] < 0:
-            fw_side = 'BUY'
-        else:
-            fw_side = None
-        order_side = (
-            self._reverse_side(fw_side) if self.contrary else fw_side
-        )
-        self.logger.info(
-            'bollinger_band: {0}, order_side: {1}'.format(
-                bollinger_band, order_side
-            )
-        )
-        return order_side
-
     def _compute_order_size(self):
-        if not self.open:
-            bet_size = self.reserved['size']
-        elif self.last_open and self.n_size_over == 1:
-            bet_size = self.last_open['size']
-        elif self.last_open and self.n_size_over == 0:
-            betting_system = self.trade.get('bet')
-            if not self.won and betting_system == 'Martingale':
-                m = (self.trade['size'].get('multiplier') or 2)
-                bet_size = (
-                    self.last_open['size'] * m
-                    if self.margin < self.last_open['margin'] else
-                    self.last_open['size'] / m
-                )
-            elif not self.won and betting_system == "d'Alembert":
-                a = self.trade['size']['unit'] * (
-                    self.trade['size'].get('multiplier') or 1
-                )
-                bet_size = (
-                    self.last_open['size'] + a
-                    if self.margin < self.last_open['margin'] else
-                    self.last_open['size'] - a
-                )
+        if self.open:
+            if self.last_open and self.n_size_over == 1:
+                bet_size = self.last_open['size']
+            elif self.last_open and self.n_size_over == 0:
+                betting_system = self.trade.get('bet')
+                if not self.won and betting_system == 'Martingale':
+                    bet_size = self.last_open['size'] * (
+                        self.trade['size'].get('multiplier') or 2
+                    )
+                elif not self.won and betting_system == "d'Alembert":
+                    bet_size = self.last_open['size'] + (
+                        self.trade['size']['unit'] * (
+                            self.trade['size'].get('multiplier') or 1
+                        )
+                    )
+                else:
+                    bet_size = self.trade['size']['unit']
             else:
                 bet_size = self.trade['size']['unit']
+            self.logger.info('bet_size: {}'.format(bet_size))
+            order_size = math.ceil(
+                (
+                    min(bet_size, self.trade['size']['max'])
+                    if self.open and 'max' in self.trade['size'] else bet_size
+                ) * 1000
+            ) / 1000
         else:
-            bet_size = self.trade['size']['unit']
-        order_size = math.ceil(
-            (
-                min(bet_size, self.trade['size']['max'])
-                if self.open and 'max' in self.trade['size'] else bet_size
-            ) * 1000
-        ) / 1000
-        self.logger.info(
-            'bet_size: {0}, order_size: {1}'.format(bet_size, order_size)
-        )
+            order_size = self.reserved['size']
+        self.logger.info('order_size: {}'.format(order_size))
         return order_size
 
     def _trade(self):
@@ -277,21 +280,11 @@ class BfStreamTrader(SubscribeCallback):
             self.reserved = self.position
             self.order_datetime = None
         self.logger.info('self.reserved: {}'.format(self.reserved))
-        if self.anchor_margin:
-            self.logger.info(
-                'pl in round: {}'.format(self.margin - self.anchor_margin)
-            )
-            self.won = (self.margin >= self.anchor_margin)
-            self.logger.info('self.won: {}'.format(self.won))
-            if self.won and self.reserved['size'] == 0 and not queue_is_left:
-                self.anchor_margin = 0
-            else:
-                pass
-            self.logger.info(
-                'self.anchor_margin: {}'.format(self.anchor_margin)
-            )
+        if self.last_open and not queue_is_left:
+            self.won = (self.margin > self.last_open['margin'])
         else:
-            self.logger.debug('self.won: {}'.format(self.won))
+            pass
+        self.logger.info('self.won: {}'.format(self.won))
         self.open = (self.reserved['size'] < self.trade['size']['unit'])
         order_size = self._compute_order_size()
 
@@ -347,15 +340,6 @@ class BfStreamTrader(SubscribeCallback):
                             'side': self.order_side,
                             'size': self.reserved['size'] + order_size
                         }
-                        if self.anchor_margin:
-                            pass
-                        else:
-                            self.anchor_margin = self.margin
-                            self.logger.info(
-                                'self.anchor_margin: {}'.format(
-                                    self.anchor_margin
-                                )
-                            )
                     else:
                         updated_size = self.reserved['size'] - order_size
                         if abs(updated_size) < 0.001:
@@ -371,20 +355,27 @@ class BfStreamTrader(SubscribeCallback):
                                 'side': self._reverse_side(self.order_side),
                                 'size': abs(updated_size)
                             }
-                        self.ewm_pl = self._compute_ewm_pl()
-                        if self.mode == 'pivot' and self.ewm_pl['mean'] < 0:
-                            self.contrary = not self.contrary
-                            self.logger.info(
-                                'self.contrary: {}'.format(self.contrary)
-                            )
+                        if self.mode == 'pivot':
+                            self.ewm_pl = self._compute_ewm_pl()
+                            if self.ewm_pl['mean'] < 0:
+                                self.contrary = not self.contrary
+                                self.logger.info(
+                                    'self.contrary: {}'.format(self.contrary)
+                                )
+                            else:
+                                pass
                         else:
                             pass
+                    self.retried_side = None
                     self.n_size_over = 0
                 else:
-                    self.n_size_over += int(
-                        'status' in order and order['status'] == - 205
-                    )
-                    self.logger.warning(os.linesep + dump_yaml(order))
+                    if order.get('status') == - 205:
+                        self.n_size_over += 1
+                    elif order.get('status') in [- 1, - 208, - 508]:
+                        self.retried_side = self.order_side
+                    else:
+                        pass
+                    self.logger.error(os.linesep + dump_yaml(order))
 
 
 def open_deal(config, pair, timeout=3600, quiet=False):
